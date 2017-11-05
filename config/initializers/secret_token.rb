982798d22d5ff4465b07801d4f914924224d8a6a
Ashesh Vidyut <ashesh.vidyut@splashmath.com>
2017-11-05 19:25:36 +0530

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DtcApp::Application.config.secret_token = 'bdc94b5d3f13b225f092600e35d3b4ac9595310a16423575ca0733d8c1423b40ab7c013fae99bc74ab36fef92cd82f463921e766be0bdd8c34651f670abb4dfc'
